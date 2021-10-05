1cb1591fd4878313bc6e95cf728e0d05bbb6e6f9
chenyongping <chenyongping001@gmail.com>
2021-10-06 06:29:46 +0800

    extend User and user profile

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,10 @@
 from store.models import Product
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from store.admin import ProductAdmin
 from tags.models import TaggedItem
+from .models import User
 
 
 class TagInline(GenericTabularInline):
@@ -16,3 +18,13 @@ class CustomProductAdmin(ProductAdmin):
 
 admin.site.unregister(Product)
 admin.site.register(Product, CustomProductAdmin)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'email', 'first_name', 'last_name'),
+        }),
+    )
--- storefront/settings.py
+++ storefront/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',
     'store',
-    'store_custom',
+    'core',
     'tags',
     'likes'
 ]
@@ -147,3 +147,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
 }
+
+AUTH_USER_MODEL = 'core.User'
